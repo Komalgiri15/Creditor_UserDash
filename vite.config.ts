85ea40f5c4dcc5be601229c4ee1e3e62a737c634
Ayush Vaishy <ayuvaishy@gmail.com>
2025-08-07 13:53:51 +0530

    env updated

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,6 @@ export default defineConfig(({ mode }) => ({
     },
   },
   define: {
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('https://creditor-backend-testing-branch.onrender.com'),
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('https://creditor-backend-9upi.onrender.com'),
   },
 }));
